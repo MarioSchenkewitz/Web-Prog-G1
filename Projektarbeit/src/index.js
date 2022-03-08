ab655e2440422dfce83296b822940467f3797534
MarioSchenkewitz <90987509+MarioSchenkewitz@users.noreply.github.com>
2022-03-08 21:08:00 +0100

    Changed cookie creation

--- Projektarbeit/src/index.js
+++ Projektarbeit/src/index.js
@@ -1,7 +1,7 @@
 // wir benötigen http funktionen
 const express = require("express");
 // wird benötigt um formular daten lesen zu können
- const bodyParser = require("body-parser");
+const bodyParser = require("body-parser");
 //cookies
 const cookieParser = require("cookie-parser");
 //unique identifier for users
@@ -15,52 +15,45 @@ const port = 8080;
 
 app.use(cookieParser());
 
-
-// bitte stelle alle Dateien auf dem Verzeichnis public zur Verfügung
-app.use(express.static("public"));
-
 // express js muss wissen, dass es den body-parser verwenden soll
- app.use(bodyParser.json());
- app.use(bodyParser.urlencoded());
- app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 
-app.get("/comment", getComment);
-app.post("/comment", addComment);
-
-const user = "";
-
-app.get("/", (req, res) => {
-  console.log("currently in /");
+function userCheck(req, res, next) {
   if (req.cookies.username) {
     console.log("user is logged in");
-    res.redirect("/home.html");
   } else {
     console.log("not logged in");
-    res.redirect("/createUser?username=" + uuidv4());
+    res.cookie("username", uuidv4(), { maxAge: 900000, httpOnly: true })
   }
-});
+  next()
+}
 
-app.get("/index.html", (req, res) => {
-  res.redirect("/");
-});
+app.use(userCheck);
 
-app.get("/createUser", (req, res) => {
-  if (req.query.username) {
-    res.cookie("username", req.query.username).redirect("/home.html");
-  }
-});
+app.use("/", userCheck, express.static("./public"))
 
+
+app.get("/test",userCheck, (req, res) => {
+
+});
 
 // und starte den server und lausche auf Port 8080
 app.listen(port, () => {
   console.log(`Katzopedia app listening on port ${port}`);
 });
 
+app.get("/funfacts/comment", getComment);
+app.post("/funfacts/comment", addComment);
+
 const comments = [];
 
 function addComment(req, res) {
   if (req.body.comments) {
-    cities.push(req.cookies.username + ": " + req.body);
+    comments.push(req.cookies.username + ": " + req.body);
   }
   console.log(comments);
   res.redirect("/");
